ff1524d5c7641dd4fad82411292908ecf93b6c20
tui_2019 <tui2019@tutanota.com>
2024-10-28 14:01:40 +0200

    Added function to get answer.

--- test_solver/solver.js
+++ test_solver/solver.js
@@ -1,7 +1,8 @@
 var questions = document.getElementsByClassName("geS5n");
 for (var i = 0; i < questions.length; i++) {
   var question = questions[i];
-  var questionText =
-    question.firstChild.firstChild.firstChild.firstChild.innerText;
-  alert(questionText);
+  var questionText = question.firstChild.firstChild.firstChild.firstChild;
+  questionText.ondblclick = function () {
+    alert(this.innerText);
+  };
 }
