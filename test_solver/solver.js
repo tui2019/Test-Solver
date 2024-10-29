8c68046eaae1bdff72fb970d44ed8ab8e8dbd561
tui_2019 <tui2019@tutanota.com>
2024-10-29 17:33:56 +0200

    Added fetching of answer options

--- test_solver/solver.js
+++ test_solver/solver.js
@@ -1,8 +1,20 @@
+function get_ansers(question, answers) {
+  alert("Question: " + question + "\n\nAnswers:\n" + answers.join("\n"));
+}
+
 var questions = document.getElementsByClassName("geS5n");
 for (var i = 0; i < questions.length; i++) {
   var question = questions[i];
-  var questionText = question.firstChild.firstChild.firstChild.firstChild;
+  // var questionText = question.firstChild.firstChild.firstChild.firstChild;
+  var questionText = question.getElementsByClassName("M7eMe")[0];
+
   questionText.ondblclick = function () {
-    alert(this.innerText);
+    var answers = [];
+    for (var j = 0; j < this.parentElement.parentElement.parentElement.parentElement.parentElement.getElementsByClassName("nWQGrd").length; j++) {
+      answers.push(
+        this.parentElement.parentElement.parentElement.parentElement.parentElement.getElementsByClassName("nWQGrd")[j].getElementsByTagName("span")[0].innerText,
+      );
+    }
+    get_ansers(this.innerText, answers);
   };
 }
