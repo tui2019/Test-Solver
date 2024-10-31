80c23316755d57fc04ccec456892448f535325fb
tui_2019 <tui2019@tutanota.com>
2024-10-31 18:10:19 +0200

    Some bug fixes, still can't get API working

--- test_solver/solver.js
+++ test_solver/solver.js
@@ -1,26 +1,27 @@
-// Fetch your API_KEY
-var key = "insert_your_key_here";
+// import { GoogleGenerativeAI } from "https://esm.run/@google/generative-ai";
+// import('https://esm.run/@google/generative-ai')
+//   .then(module => {
+//     const { GoogleGenerativeAI } = module;
+//   })
 
-function get_ansers(question, answers) {
-  alert("Question: " + question + "\n\nAnswers:\n" + answers.join("\n"));
-}
-
-
-// inserting required scripts into html
 // https://aistudio.google.com
 // Use VPN to generate API key
 // https://ai.google.dev/gemini-api/docs/quickstart?lang=web
-var head = document.getElementsByTagName("head")[0];
-var script1 = document.createElement("script");
-script1.type = "importmap";
-script1.innerHTML = '{"imports": {"@google/generative-ai": "https://esm.run/@google/generative-ai"}}'
+const API_KEY = "insert_your_key_here";
 
-var script2 = document.createElement("script");
-script2.type = "module";
-script2.innerHTML = 'import { GoogleGenerativeAI } from "@google/generative-ai"; const API_KEY = ' + key + ';'
 
-head.appendChild(script1);
-head.appendChild(script2);
+function get_ansers(question, answers) {
+  alert("Question: " + question + "\nAnswers:\n" + answers.join("\n"));
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  const prompt = "Write a story about a magic backpack.";
+
+  const result = await model.generateContent(prompt);
+  alert(result.response.text());
+}
+
+
 
 // getting questions and answers and creating event listener
 var questions = document.getElementsByClassName("geS5n");
