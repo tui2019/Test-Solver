d23e217090eeca30525597b45e85501290b13fad
tui <tui@tuis-MacBook-Air.local>
2025-01-17 23:26:26 +0200

    Code rafactoring and bugfixes.

---
+++ test_solver/background.js
@@ -0,0 +1,27 @@
+// https://aistudio.google.com
+// https://ai.google.dev/gemini-api/docs/quickstart?lang=web
+import { GoogleGenerativeAI } from "https://esm.run/@google/generative-ai";
+var genAI = new GoogleGenerativeAI("YOUR_API_KEY");
+var model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-exp" });
+
+
+
+async function get_answers(request, sender, sendResponse) {
+  var answers_str = "";
+  for (var i = 0; i < request.answers.length; i++) {
+    if (i == 0) {
+      answers_str += "1.";
+    } else {
+      answers_str += ` ${i + 1}.`;
+    }
+    answers_str += request.answers[i];
+  }
+  var prompt_str = "Допоможи вирішити завдання та дай відповідь лише цифрою(1, 2, 3...): " + request.question + ". Варіанти відповідей: " + answers_str;
+      var result = await model.generateContent(prompt_str);
+      var responseText = await result.response.text();
+      // sendResponse({ response: responseText });
+    return responseText; // Indicate that sendResponse will be called asynchronously
+  }
+
+
+browser.runtime.onMessage.addListener(get_answers);
--- test_solver/solver.js
+++ test_solver/solver.js
@@ -1,26 +1,10 @@
-// https://aistudio.google.com
-// Use VPN to generate and use API key
-// https://ai.google.dev/gemini-api/docs/quickstart?lang=web
-const API_KEY = "key_here";
-
-
-function get_ansers(question, answers) {
-  alert("Question: " + question + "\nAnswers:\n" + answers.join("\n"));
-
-  (async () => {
-    const { GoogleGenerativeAI } = await import("https://esm.run/@google/generative-ai");
-
-    const genAI = new GoogleGenerativeAI(API_KEY);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-    const prompt = "Write a story about a magic backpack.";
-
-    const result = await model.generateContent(prompt);
-    alert(result.response.text());
-  })();
+function handleResponse(response) {
+  alert(response);
 }
 
-
+function handleError(error) {
+  console.log(`Background error: ${error}`);
+}
 
 // getting questions and answers and creating event listener
 var questions = document.getElementsByClassName("geS5n");
@@ -31,10 +15,12 @@ for (var i = 0; i < questions.length; i++) {
   questionText.ondblclick = function () {
     var answers = [];
     for (var j = 0; j < this.parentElement.parentElement.parentElement.parentElement.parentElement.getElementsByClassName("nWQGrd").length; j++) {
-      answers.push(
-        this.parentElement.parentElement.parentElement.parentElement.parentElement.getElementsByClassName("nWQGrd")[j].getElementsByTagName("span")[0].innerText,
-      );
+      answers.push(this.parentElement.parentElement.parentElement.parentElement.parentElement.getElementsByClassName("nWQGrd")[j].getElementsByTagName("span")[0].innerText);
     }
-    get_ansers(this.innerText, answers);
+    const ansers_request = browser.runtime.sendMessage({
+        question: this.innerText,
+        answers: answers,
+      });
+      ansers_request.then(handleResponse, handleError);
   };
-}
+};
