463b7ac18507cb6821fe6bb2115e84ba60d765e2
Тюльпін Любомир <61665468+tui2019@users.noreply.github.com>
2025-01-23 17:00:52 +0200

    Added support for text questions

--- test_solver/background.js
+++ test_solver/background.js
@@ -1,12 +1,17 @@
 // https://aistudio.google.com
 // https://ai.google.dev/gemini-api/docs/quickstart?lang=web
 import { GoogleGenerativeAI } from "https://esm.run/@google/generative-ai";
-var genAI = new GoogleGenerativeAI("Your api Key");
+var genAI = new GoogleGenerativeAI("YOUR_API_KEY");
 var model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-exp" });
 
 
 
 async function get_answers(request, sender, sendResponse) {
+  if (request.answers == "text_question") {
+    var prompt_str = "Дай стислу відповідь (слово без пояснееня, якщо можливо і коротке речення, якщо ні): " + request.question;
+    var result = await model.generateContent(prompt_str);
+    var responseText = await result.response.text();
+  } else {
   var answers_str = "";
   for (var i = 0; i < request.answers.length; i++) {
     if (i == 0) {
@@ -16,16 +21,10 @@ async function get_answers(request, sender, sendResponse) {
     }
     answers_str += request.answers[i];
   }
-  if(answers_str.length<=0){
-    var prompt_str = "Допоможи вирішити завдання дай лаконічну, достатню відповідь на наступне запитання " + request.question;
-      
-  }
-  else{
   var prompt_str = "Допоможи вирішити завдання та дай відповідь лише цифрою(1, 2, 3...) можливі декілька правильних відповідей, без жодних інших слів: " + request.question + ". Варіанти відповідей: " + answers_str;
-  }
       var result = await model.generateContent(prompt_str);
       var responseText = await result.response.text();
-      // sendResponse({ response: responseText });
+  }
     return responseText; // Indicate that sendResponse will be called asynchronously
   }
 
