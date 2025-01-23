ad67f11572ff522bfa9b449f7a096865a65a2ed5
danbur08 <95135771+danbur08@users.noreply.github.com>
2025-01-23 16:33:26 +0200

    Add files via upload

--- test_solver/background.js
+++ test_solver/background.js
@@ -1,7 +1,7 @@
 // https://aistudio.google.com
 // https://ai.google.dev/gemini-api/docs/quickstart?lang=web
 import { GoogleGenerativeAI } from "https://esm.run/@google/generative-ai";
-var genAI = new GoogleGenerativeAI("Api Key");
+var genAI = new GoogleGenerativeAI("API KEY");
 var model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-exp" });
 
 
@@ -16,7 +16,12 @@ async function get_answers(request, sender, sendResponse) {
     }
     answers_str += request.answers[i];
   }
+  if(answers_str.len<=0){
+    var prompt_str = "Допоможи вирішити завдання та дай розгорнуту відповідь на наступне запитання " + request.question;  
+  }
+  else{
   var prompt_str = "Допоможи вирішити завдання та дай відповідь лише цифрою(1, 2, 3...) можливі декілька правильних відповідей, без жодних інших слів: " + request.question + ". Варіанти відповідей: " + answers_str;
+  }
       var result = await model.generateContent(prompt_str);
       var responseText = await result.response.text();
       // sendResponse({ response: responseText });
--- test_solver/solver.js
+++ test_solver/solver.js
@@ -1,10 +1,17 @@
 function handleResponse(response) {
+  const regex = /[^0-9\s,]/;
+  if(regex.test(response)){
+    alert(response+ "<< PLEASE COPY THAT TEXT AND PASTE IT INTO THE TEXT BOX >>")   
+  }
+  else{
   const responseArray = response.split(", ").map(Number);
   for (let i=0; i<responseArray.length;i++){
     var correct_answer = answers_HTML[responseArray[i] -1];
     correct_answer.click();
   }
+ 
   
+}
 }
 
 function handleError(error) {
@@ -14,7 +21,7 @@ function handleError(error) {
 // getting questions and answers and creating event listener
 //
 var answers_HTML = [];
-
+var testText=null;
 var questions = document.getElementsByClassName("geS5n");
 for (var i = 0; i < questions.length; i++) {
   var question = questions[i];
@@ -31,6 +38,7 @@ for (var i = 0; i < questions.length; i++) {
       answers.push(this.parentElement.parentElement.parentElement.parentElement.parentElement.getElementsByClassName("eBFwI")[j].getElementsByTagName("span")[0].innerText);
       answers_HTML.push(this.parentElement.parentElement.parentElement.parentElement.parentElement.getElementsByClassName("eBFwI")[j].firstChild);
     }
+  
     const ansers_request = browser.runtime.sendMessage({
       question: this.innerText,
       answers: answers,
