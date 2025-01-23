f2b142cd719e600f542230a45021040416952e96
danbur08 <95135771+danbur08@users.noreply.github.com>
2025-01-23 16:49:08 +0200

    Add files via upload

--- test_solver/background.js
+++ test_solver/background.js
@@ -1,7 +1,7 @@
 // https://aistudio.google.com
 // https://ai.google.dev/gemini-api/docs/quickstart?lang=web
 import { GoogleGenerativeAI } from "https://esm.run/@google/generative-ai";
-var genAI = new GoogleGenerativeAI("API KEY");
+var genAI = new GoogleGenerativeAI("Your api Key");
 var model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-exp" });
 
 
@@ -16,8 +16,9 @@ async function get_answers(request, sender, sendResponse) {
     }
     answers_str += request.answers[i];
   }
-  if(answers_str.len<=0){
-    var prompt_str = "Допоможи вирішити завдання та дай розгорнуту відповідь на наступне запитання " + request.question;  
+  if(answers_str.length<=0){
+    var prompt_str = "Допоможи вирішити завдання дай лаконічну, достатню відповідь на наступне запитання " + request.question;
+      
   }
   else{
   var prompt_str = "Допоможи вирішити завдання та дай відповідь лише цифрою(1, 2, 3...) можливі декілька правильних відповідей, без жодних інших слів: " + request.question + ". Варіанти відповідей: " + answers_str;
