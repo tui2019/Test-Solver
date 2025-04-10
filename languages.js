4050669835198bff83cc9a4b1736f003b7aee213
tui <tui@tuis-MacBook-Air.local>
2025-04-10 22:27:00 +0300

    Fixed missing files

---
+++ languages.js
@@ -0,0 +1,48 @@
+const translations = {
+  "en": {
+    "settings": {
+      "apiKeyPlaceholder1": "API Key 1",
+      "apiKeyPlaceholder2": "API Key 2 (optional)",
+      "getApiKey": "Get a Gemini API key here.",
+      "apiKeyInfo": "Providing 2 API keys increases your request limit.",
+      "enableExperimentalLogic": "Enable experimental logic",
+      "languageSelector": "Language"
+    },
+    "prompts": {
+      "giveAnswer": "Give an answer (if possible - in one word): ",
+      "solveProblemNumberOnly": "Help solve this problem and give the answer only as a number (1, 2, 3...): ",
+      "answerOptions": "Answer options: ",
+      "solveProblemFormat": "Help solve this problem and give the answer in format (1A, 2B, 3C...) without any extra words: ",
+      "questions": "Questions: ",
+      "solveProblemMultiple": "Help solve this problem and give the answer only as a number (1, 2, 3..., without any other words). Multiple correct answers are possible: "
+    },
+    "alphabet": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "connectors": {
+      "and": " and "
+    }
+  },
+  "uk": {
+    "settings": {
+      "apiKeyPlaceholder1": "API Ключ 1",
+      "apiKeyPlaceholder2": "API Ключ 2 (необов'язково)",
+      "getApiKey": "Отримати ключ Gemini API можна тут.",
+      "apiKeyInfo": "Надання 2 API ключів збільшує ліміт ваших запитів.",
+      "enableExperimentalLogic": "Увімкнути експериментальну логіку",
+      "languageSelector": "Мова"
+    },
+    "prompts": {
+      "giveAnswer": "Дай відповідь (якщо можливо - одним словом): ",
+      "solveProblemNumberOnly": "Допоможи вирішити завдання та дай відповідь лише цифрою(1, 2, 3...): ",
+      "answerOptions": "Варіанти відповідей: ",
+      "solveProblemFormat": "Допоможи вирішити завдання та дай відповідь в форматі (1А, 2Б, 3В...) без ніяких зайвих слів: ",
+      "questions": "Питання: ",
+      "solveProblemMultiple": "Допоможи вирішити завдання та дай відповідь лише цифрою(1, 2, 3..., без жодних інших слів). Можливі декілька правильних відповідей: "
+    },
+    "alphabet": "АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ",
+    "connectors": {
+      "and": " та "
+    }
+  }
+};
+
+export default translations;
